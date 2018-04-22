bdc41a12f3d625e5edbb7177c9670d2c7a24e6f0
Birger <birgersp@gmail.com>
2018-04-22 12:39:18 +0100

    add Timestamp class

---
+++ include/cpputil/TimeStamp.hpp
@@ -0,0 +1,61 @@
+#ifndef TIMESTAMP_HPP
+#define TIMESTAMP_HPP
+
+#include <sys/time.h>
+#include <cpputil/core.hpp>
+
+namespace cpputil
+{
+
+class Timestamp
+{
+public:
+
+    Timestamp()
+    {
+        gettimeofday(&timeValue, 0);
+        timeValueSeconds = timeValue.tv_sec;
+        values = localtime(&timeValueSeconds);
+    }
+
+    const int year() const
+    {
+        return values->tm_year;
+    }
+
+    const int month() const
+    {
+        return values->tm_mon + 1;
+    }
+
+    const int day() const
+    {
+        return values->tm_mday;
+    }
+
+    const int hour() const
+    {
+        return values->tm_hour;
+    }
+
+    const int minute() const
+    {
+        return values->tm_min;
+    }
+
+    const ulong secondsSinceEpoch() const
+    {
+        return timeValueSeconds;
+    }
+
+private:
+
+    timeval timeValue;
+    time_t timeValueSeconds;
+    tm* values;
+
+};
+
+}
+
+#endif /* TIMESTAMP_HPP */
