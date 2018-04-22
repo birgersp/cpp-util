20b8b1fac7d9af3bebe59aa96874e23c69520d73
Birger <birgersp@gmail.com>
2018-04-22 12:15:33 +0100

    add string util features

---
+++ include/cpputil/string.hpp
@@ -0,0 +1,39 @@
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#include <vector>
+#include <string>
+
+namespace cpputil
+{
+
+std::vector<std::string> splitString(const std::string& string, char delimiter)
+{
+    std::vector<std::string> result;
+
+    if (string != "")
+    {
+        int i1 = 0, i2;
+        i2 = string.find(delimiter);
+        while (i2 != -1)
+        {
+            result.push_back(string.substr(i1, i2 - i1));
+            i1 = i2 + 1;
+            i2 = string.find(delimiter, i1);
+        }
+        result.push_back(string.substr(i1));
+    }
+
+    return result;
+}
+
+void toLowerCase(std::string& string)
+{
+    std::locale locale;
+    for (uint i = 0; i < string.length(); i++)
+        string[i] = std::tolower(string[i], locale);
+}
+
+}
+
+#endif /* STRING_HPP */
