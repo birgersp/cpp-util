79eeace2cac07da88f442e8bebe57990c910bf06
Birger Skogeng Pedersen <birgersp@gmail.com>
2018-08-30 20:58:27 +0200

    add stringcompare function

--- include/cpputil/string.hpp
+++ include/cpputil/string.hpp
@@ -36,6 +36,57 @@ void toLowerCase(MutableStringRef string)
         string[i] = std::tolower(string[i], locale);
 }
 
+namespace stringcompare
+{
+
+struct StringDifference
+{
+    uint line;
+    std::string string1Line, string2Line;
+};
+
+bool findStringDifference(StringRef string1, StringRef string2, StringDifference& difference)
+{
+    std::vector<std::string> string1Lines = splitString(string1, '\n');
+    std::vector<std::string> string2Lines = splitString(string2, '\n');
+
+    if (string1Lines.size() > string2Lines.size())
+    {
+        difference.line = string2Lines.size() + 1;
+        difference.string1Line = string1Lines[string2Lines.size()];
+        difference.string2Line = "";
+        return true;
+    }
+
+    if (string2Lines.size() > string1Lines.size())
+    {
+        difference.line = string1Lines.size() + 1;
+        difference.string1Line = "";
+        difference.string2Line = string2Lines[string1Lines.size()];
+        return true;
+    }
+
+    uint noOfLines = string1Lines.size();
+
+    uint lineIndex = 0;
+    while (lineIndex < noOfLines)
+    {
+        StringRef string1Line = string1Lines[lineIndex];
+        StringRef string2Line = string2Lines[lineIndex];
+        if (string1Line != string2Line)
+        {
+            difference.line = lineIndex + 1;
+            difference.string1Line = string1Line;
+            difference.string2Line = string2Line;
+            return true;
+        }
+        lineIndex++;
+    }
+    return false;
+}
+
+}
+
 }
 
 #endif /* STRING_HPP */
