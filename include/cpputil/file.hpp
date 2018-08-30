e0fd5d8c1b53ad3e3e0719b0440322fc68606f6f
Birger Skogeng Pedersen <birgersp@gmail.com>
2018-08-30 18:25:38 +0200

    add readFile function

---
+++ include/cpputil/file.hpp
@@ -0,0 +1,33 @@
+#ifndef FILE_HPP
+#define FILE_HPP
+
+#include <fstream>
+
+namespace cpputil
+{
+
+bool readFile(StringRef filename, MutableStringRef data)
+{
+    std::ifstream infile(filename);
+
+    data.clear();
+
+    if (infile.is_open())
+    {
+        std::string line;
+        if (std::getline(infile, line))
+        {
+            data += line;
+            while (std::getline(infile, line))
+                data += "\n" + line;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+}
+
+#endif /* FILE_HPP */
+
