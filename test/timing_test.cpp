3db9baeff51dc99a5d8c67baf720e26aef8839b4
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-04-01 21:09:55 +0200

    fix timing tests

--- test/timing_test.cpp
+++ test/timing_test.cpp
@@ -8,15 +8,19 @@ namespace tests
 
 void testTiming1()
 {
+    sleepMS(10);
+
     Timer timer;
 
     sleepMS(10);
-    assertTrue(timer.elapsedMS() >= 10 && timer.elapsedMS() <= 11);
+    assertTrue(timer.elapsedMS() >= 10);
+    assertTrue(timer.elapsedMS() <= 50);
 
     timer.restart();
     assertEquals(0, timer.elapsedMS());
     sleepMS(5);
-    assertTrue(timer.elapsedMS() >= 5 && timer.elapsedMS() <= 6);
+    assertTrue(timer.elapsedMS() >= 5);
+    assertTrue(timer.elapsedMS() <= 45);
     assertTrue(timer.elapsedMS() > 0);
 }
 
