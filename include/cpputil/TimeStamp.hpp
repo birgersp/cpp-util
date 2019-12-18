59469301a1762e79218b22737506b10fec689031
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-12-18 19:56:00 +0100

    fix: day of week 0 is sunday

--- include/cpputil/TimeStamp.hpp
+++ include/cpputil/TimeStamp.hpp
@@ -27,13 +27,13 @@ public:
 	{
 		switch (integer)
 		{
+		case 0: return DayOfWeek::SUNDAY;
 		case 1: return DayOfWeek::MONDAY;
 		case 2: return DayOfWeek::TUESDAY;
 		case 3: return DayOfWeek::WEDNESDAY;
 		case 4: return DayOfWeek::THURSDAY;
 		case 5: return DayOfWeek::FRIDAY;
 		case 6: return DayOfWeek::SATURDAY;
-		case 7: return DayOfWeek::SUNDAY;
 		}
 	}
 
