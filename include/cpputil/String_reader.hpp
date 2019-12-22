a191c5a2b2f908ee724821d68b9bf446fe9a6c0c
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-12-22 15:06:38 +0100

    implement string reader functions

--- include/cpputil/String_reader.hpp
+++ include/cpputil/String_reader.hpp
@@ -16,20 +16,48 @@ public:
 		char leading_character;
 	};
 
-	String_reader(String_ref string)
+	String_reader(String string) :
+	string(string)
 	{
 	}
 
 	bool at_end() const
 	{
-		return false;
+		return index >= string.length();
 	}
 
-	Result read_until(const Vector<char>&)
+	Result read_until(const Vector<char>& characters)
 	{
+		buffer.clear();
 		Result result;
+		bool done = false;
+		while (not (done or at_end()))
+		{
+			char current_character = string[index];
+			for (char character : characters)
+			{
+				if (character == current_character)
+				{
+					done = true;
+					result.leading_character = character;
+					break;
+				}
+			}
+			if (not done)
+			{
+				buffer += current_character;
+			}
+			index++;
+		}
+		result.string = buffer;
 		return result;
 	}
+
+private:
+
+	String string, buffer;
+	uint index = 0;
+
 };
 
 }
